1e04bba023e7f9cd5cfc172be542d9f23cdecc03
Saber Hamidi <sh30g12@soton.ac.uk>
2016-01-28 00:26:20 +0000

    Product

---
+++ app/Test/Fixture/ProductFixture.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Product Fixture
+ */
+class ProductFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'length' => 11, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false),
+		'description' => array('type' => 'text', 'null' => true),
+		'price' => array('type' => 'text', 'null' => false, 'default' => '0.00', 'length' => 10),
+		'category_id' => array('type' => 'text', 'null' => false, 'length' => 11),
+		'created' => array('type' => 'datetime', 'null' => false),
+		'modified' => array('type' => 'datetime', 'null' => false),
+		'indexes' => array(
+			
+		),
+		'tableParameters' => array()
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'price' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'category_id' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'created' => '2016-01-28 00:24:09',
+			'modified' => '2016-01-28 00:24:09'
+		),
+	);
+
+}
